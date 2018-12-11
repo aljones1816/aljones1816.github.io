135e2457925c9e7a8b9a1254711295fe59a58ad7
Alan <30361001+aljones1816@users.noreply.github.com>
2018-12-11 15:15:42 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,5 +30,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+  
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 });
