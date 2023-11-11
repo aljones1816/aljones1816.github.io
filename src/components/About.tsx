13b4dbb0b2e7a670a39fe2180608041a9a377401
Alan <aljones1816@gmail.com>
2023-11-11 02:25:59 -0500

    fixed some style issues

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,26 +2,39 @@ function About() {
   return (
     <div className="about-me">
       <p>
-        My journey into software began in 2015 when, as a graduate student
-        studying paleoclimatology, I began tinkering with R programs used to
-        unravel the timing of past great climate changes recorded in ocean
-        sediment cores. Since then, I've been lucky to have collaborated with
-        Massachusetts energy utilities to develop data platforms that drive
-        their energy efficiency programs, and to participate in a large-scale
-        data warehouse migration.
+        My journey into software began in 2015 when, as a graduate student{" "}
+        <a
+          href="https://www.sciencedirect.com/science/article/pii/S0277379122001901"
+          className="about-link"
+        >
+          studying paleoclimatology
+        </a>
+        , I began tinkering with R programs used to unravel the timing of past
+        great climate changes recorded in ocean sediment cores. Since then, I've
+        been lucky to have collaborated with Massachusetts energy utilities to
+        develop a{" "}
+        <a
+          href="https://viewer.dnv.com/macustomerprofile/entity/1444/report/2078"
+          className="about-link"
+        >
+          data platform
+        </a>{" "}
+        that drives their energy efficiency programs, and to participate in a
+        large-scale data warehouse migration.
       </p>
       <p>
-        My goal is to transition into full stack web development. I find this
-        space fascinating, and keeping up with the relentless innovation is
-        exciting. Below you will find a selection of personal projects, mostly
-        built with Typescript.
+        My goal is to transition full time into full stack web development. I
+        find this space fascinating, and keeping up with the relentless pace of
+        innovation is exciting. Below you will find a selection of my personal
+        projects, mostly built with Typescript.
       </p>
       <p>
-        In my free time I love spending time outdoors hiking or camping with my
-        wife and our dog, traveling to new countries, tinkering with tech at
-        home, cooking, or bouldering. My background in paleoclimate research
-        left me with an indelible drive to understand and bring awareness to the
-        reality and threat of climate change.
+        In my free time, I love being outdoors hiking or camping with my wife
+        and our dog, traveling to new countries, tinkering with tech at home,
+        cooking, weightlifting, or bouldering. My background in paleoclimate
+        research left me with an indelible drive to understand and bring
+        awareness to the reality and threat of climate change, and it is a
+        subject I'm very passionate about.
       </p>
     </div>
   );
