8d03c9c82571333b8c2c55872886403d95e8d749
Alan <aljones1816@gmail.com>
2023-12-04 20:23:34 -0500

    add new project

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -3,6 +3,7 @@ import etchASketch from "./etch-a-sketch.jpg";
 import simpleTemp from "./simple-temp.jpg";
 import gameOfLife from "./game-of-life.jpg";
 import ticTacToe from "./tictactoe.jpg";
+import apiResumeThumb from "./apiResumeThumb.jpg";
 
 const projects = [
   {
@@ -16,7 +17,16 @@ const projects = [
   },
   {
     id: 2,
-    title: "John Conway's Game of Life",
+    title: "API Resume",
+    description: "My resume as a REST API.",
+    imageUrl: apiResumeThumb,
+    repoUrl: "https://github.com/aljones1816/api-resume",
+    liveUrl: "https://resume.alanjones.dev/api/docs/",
+    techStack: ["Postgres", "Express", "Node", "Swagger"],
+  },
+  {
+    id: 3,
+    title: "John Conway's Game of Life.",
     description: "An interactive cellular automation game",
     imageUrl: gameOfLife,
     repoUrl: "",
@@ -24,7 +34,7 @@ const projects = [
     techStack: ["Typescript", "React", "Webpack"],
   },
   {
-    id: 3,
+    id: 4,
     title: "SimpleTemp Global",
     description: "A global surface temperature analysis.",
     imageUrl: simpleTemp,
@@ -33,18 +43,18 @@ const projects = [
     techStack: ["Python"],
   },
   {
-    id: 4,
+    id: 5,
     title: "Tic-Tac-Toe",
-    description: "Two player tic-tac-toe",
+    description: "Two player tic-tac-toe.",
     imageUrl: ticTacToe,
     repoUrl: "https://github.com/aljones1816/tic-tac-toe/tree/main",
     liveUrl: "https://alanjones.dev/tic-tac-toe/",
     techStack: ["Javascript", "HTML5", "CSS"],
   },
   {
-    id: 5,
+    id: 6,
     title: "Sketch-A-Sketch",
-    description: "A fun, retro Etch-a-Sketch app",
+    description: "A fun, retro Etch-a-Sketch app.",
     imageUrl: etchASketch,
     repoUrl: "https://github.com/aljones1816/etch-a-sketch",
     liveUrl: "https://alanjones.dev/etch-a-sketch/",
